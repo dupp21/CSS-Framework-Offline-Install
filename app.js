3e272087007f68c7d41e4e105caff0feec4956c1
Domingos Bento Pereira <luakay@gmail.com>
2019-01-03 09:46:47 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+// const items = document.getElementsByClassName("collection-item");
+// console.log(items);
+// console.log(items[0]);
+// items[0].style.color = "red";
+// items[3].textContent = "Hello";
+
+// const listItems = document
+//   .querySelector("ul")
+//   .getElementsByClassName("colection-item");
+
+// // console.log(listItems);
+
+// let lis = document.getElementsByTagName("li");
+// console.log(lis);
+// console.log(lis[0]);
+
+// lis[0].style.color = "red";
+// lis[3].textContent = "Hello";
+
+// lis = Array.from(lis);
+
+// lis.reverse();
+
+// lis.forEach(function(li, index) {
+//   console.log(li.className);
+//   li.textContent = `${index}: Hello`;
+// });
+
+// console.log(lis);
+
+const items = document.querySelectorAll("ul.collection li.collection-item");
+
+console.log(items);
